36898ade622f527f2cbdb6eb47a2a095730da33f
Jiale Hu <jhu15@ncsu.edu>
2020-05-27 17:47:26 -0400

    Expand Two Sum

--- Algorithms/src/my/algorithms/TwoSum.java
+++ Algorithms/src/my/algorithms/TwoSum.java
@@ -6,7 +6,9 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
-import java.util.stream.Stream;
+
+import my.utility.Counter;
+import my.utility.NumberReader;
 
 public class TwoSum
 {
@@ -14,12 +16,14 @@ public class TwoSum
 	public static void main(String[] args) throws FileNotFoundException
 	{
 		long[] input = NumberReader.long2array(new File("").getAbsolutePath().concat("/src/my/algorithms/testcases/2sum.txt"));
+		
+//		input = new long[]{-3, -1, 1, 2, 9, 11, 7, 6, 2};
 		System.out.println("Input Size: " + input.length);
 		
-//		long target = 25774184952L-27043105442L;
+//		long target = -1397L;
 //		List<int[]> list = twoSum(input, target);
 //		printList(input, list);
-//		System.out.println(twoSumDistinct(input, target));
+//		System.out.println(twoSumUnique(input, target));
 		
 //		int count = 0;
 //		for (int t = -10000; t <= 10000; t++)
@@ -42,10 +46,24 @@ public class TwoSum
 			targets.add(t);
 		}
 		System.out.println(targets.get(0) + " " + targets.get(targets.size()-1));
-//		targets.parallelStream().forEach(t -> {if (twoSumDistinct(input, t)) System.out.println(t);});
+		Counter counter = new Counter();
+		targets.parallelStream().forEach(t -> {
+			if (twoSumDistinct(input, t)) 
+			{
+				counter.count();
+				System.out.println(t);
+			}
+		});
+		System.out.println("Number of Distinct: " + counter.getCount());
 		
 	}
 	
+	/**
+	 * 
+	 * @param nums {@code long[]}: Array of numbers to find two numbers sum up to target
+	 * @param target {@code long}: As a target of two sum
+	 * @return {@code List<int[]>}: List of index pairs for numbers sum up to target
+	 */
 	public static List<int[]> twoSum(long[] nums, long target)
 	{
 		HashMap<Long, Integer> hashMap = new HashMap<Long, Integer>();
@@ -62,7 +80,35 @@ public class TwoSum
 		return idxList;
 	}
 	
+	/**
+	 * 
+	 * @param nums {@code long[]}: Array of numbers to find two numbers sum up to target
+	 * @param target {@code long}: As a target of two sum
+	 * @return {@code boolean true} if nums contains at least one pair of distinct numbers sum up to target
+	 */
 	public static boolean twoSumDistinct(long[] nums, long target)
+	{
+		HashSet<Long> hashSet = new HashSet<Long>();
+		for (long num : nums) {
+			long complement = target - num;
+			if (hashSet.contains(complement))
+			{
+				if (complement != num) return true;
+			} else 
+			{
+				hashSet.add(num);
+			}
+        }
+		return false;
+	}
+	
+	/**
+	 * 
+	 * @param nums {@code long[]}: Array of numbers to find two numbers sum up to target
+	 * @param target {@code long}: As a target of two sum
+	 * @return {@code boolean true} if nums contains only one pair of numbers sum up to target
+	 */
+	public static boolean twoSumUnique(long[] nums, long target)
 	{
 		HashSet<Long> hashSet = new HashSet<Long>();
 		short count = 0;
@@ -79,6 +125,12 @@ public class TwoSum
 		return count == 1;
 	}
 	
+	/**
+	 * 
+	 * {@summary }
+	 * @param nums {@code long[]}: Array of numbers to find two numbers sum up to target
+	 * @param idxList {@code List<int[]>}: List of index pairs for numbers sum up to target
+	 */
 	public static void printList(long[] nums, List<int[]> idxList)
 	{
 		System.out.println("Number of Pair: " + idxList.size());
