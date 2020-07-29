f3013c6fd66d6c7ac8478ade5240b5279d4d2e79
Jiale Hu <jhu15@ncsu.edu>
2020-07-29 00:02:43 -0400

    Implement heuristic greedy algorithm for the traveling salesman problem

--- Algorithms/src/my/algorithms/TheTravelingSalesman.java
+++ Algorithms/src/my/algorithms/TheTravelingSalesman.java
@@ -24,31 +24,56 @@ public class TheTravelingSalesman {
 	Scanner sc = new Scanner(file);
 	System.out.println(sc.nextLine());
 	
-	List<Pair<Float, Float>> cities = new ArrayList<>();
-	while (sc.hasNextFloat()) {
-	    float x = sc.nextFloat();
-	    float y = sc.nextFloat();
-	    cities.add(new Pair<Float, Float>(x, y));
+	List<Pair<Double, Double>> cities = new ArrayList<>();
+	while (sc.hasNextDouble()) {
+	    double x = sc.nextDouble();
+	    double y = sc.nextDouble();
+	    cities.add(new Pair<Double, Double>(x, y));
 	}
 	sc.close();
 	System.out.println(cities.size());
 	System.out.println(cities);
 	
-	System.out.println(dp(cities));
+	System.out.println(DP_Exact(cities));
+	System.out.println(nearestNeighbor_Heuristic(cities));
+	
+	System.out.println("----------");
+	
+	/*
+	 * [# cities]
+	 * [#] [city 1 x] [city 1 y]
+	 * ...
+	 */
+	file = new File(new File("").getAbsolutePath().concat("/src/my/algorithms/testcases/nn.txt"));
+	sc = new Scanner(file);
+	System.out.println(sc.nextLine());
+	
+	cities = new ArrayList<>();
+	while (sc.hasNextDouble()) {
+	    sc.nextDouble();
+	    double x = sc.nextDouble();
+	    double y = sc.nextDouble();
+	    cities.add(new Pair<Double, Double>(x, y));
+	}
+	sc.close();
+	System.out.println(cities.size());
+	
+	System.out.println(nearestNeighbor_Heuristic(cities));
+	
     }
     
     // O(n^2 2^n)
-    public static float dp(List<Pair<Float, Float>> set) {
+    public static double DP_Exact(List<Pair<Double, Double>> set) {
 	
 	int n = set.size();
 	if (n < 2) throw new IllegalArgumentException("Input size must be greater than 1");
 	if (n > 33) throw new IllegalArgumentException("Input size must not be greater than 33");
 	
 	// Initialize DP
-	float[][] dp = new float[1 << (n-1)][n];
+	double[][] dp = new double[1 << (n-1)][n];
 	dp[0][n-1] = 0f; // For S = {n-1}, n-1 is always selected
 	for (int i = 1; i < dp.length; i++) {
-	    dp[i][n-1] = Float.MAX_VALUE;
+	    dp[i][n-1] = Double.MAX_VALUE;
 	}
 	
 	// Construct Solution
@@ -57,15 +82,15 @@ public class TheTravelingSalesman {
 		for (int j = 0; j < n-1; j++) { // node j in subset, j != n-1
 		    if (((subset >>> j) & 1) != 1) continue; // Skip nodes not in subset
 		    
-		    float min = Float.MAX_VALUE;
+		    double min = Double.MAX_VALUE;
 		    for (int k = 0; k < n; k++) { // node k in subset, k != j
 			
 			if (k != n - 1) {
 			    if (((subset >>> k) & 1) != 1 || k == j) continue;
 			}
 			int subsetJ = subset & ~(1 << j); // Subset - {j}
-			if (dp[subsetJ][k] == Float.MAX_VALUE) continue;
-			float dist = dp[subsetJ][k] + getDist(set.get(k), set.get(j));
+			if (dp[subsetJ][k] == Double.MAX_VALUE) continue;
+			double dist = dp[subsetJ][k] + getDist(set.get(k), set.get(j));
 			min = Math.min(min, dist);
 		    }
 		    dp[subset][j] = min; // Update DP
@@ -74,22 +99,22 @@ public class TheTravelingSalesman {
 	}
 	
 	// Find Solution
-	float min = Float.MAX_VALUE;
+	double min = Double.MAX_VALUE;
 	for (int j = 0; j < n-1; j++) {
-	    float dist = dp[dp.length-1][j] + getDist(set.get(n-1), set.get(j));
+	    double dist = dp[dp.length-1][j] + getDist(set.get(n-1), set.get(j));
 	    min = Math.min(min, dist);
 	}
 	return min;
     }
     
-    private static float getDist(Pair<Float, Float> coor1, Pair<Float, Float> coor2) {
+    private static double getDist(Pair<Double, Double> coor1, Pair<Double, Double> coor2) {
 	
-	float r1 = coor1.get1() - coor2.get1();
+	double r1 = coor1.get1() - coor2.get1();
 	r1 *= r1;
-	float r2 = coor1.get2() - coor2.get2();
+	double r2 = coor1.get2() - coor2.get2();
 	r2 *= r2;
 	
-	return (float) Math.sqrt(r1 + r2);
+	return Math.sqrt(r1 + r2);
     }
 
     /**
@@ -97,7 +122,7 @@ public class TheTravelingSalesman {
      * @param set input set;
      * @return all subsets
      */
-    public static List<Set<Integer>> getSubsetsIndices(List<Pair<Float, Float>> set) {
+    public static List<Set<Integer>> getSubsetsIndices(List<Pair<Double, Double>> set) {
 	
 	List<Set<Integer>> subsets = new ArrayList<>();
 	
@@ -140,10 +165,41 @@ public class TheTravelingSalesman {
 	    return;
 	}
 	for (int i = idx; i < n-1; i++) {
-	    subset += (1 << i);
-	    helper(subsets, n, m, subset, i+1, size+1);
-	    subset -= (1 << i);
+	    helper(subsets, n, m, subset+(1 << i), i+1, size+1);
 	}
+    }
+    
+    public static double nearestNeighbor_Heuristic(List<Pair<Double, Double>> set) {
+	
+	int n = set.size();
+	if (n < 2) throw new IllegalArgumentException("Input size must be greater than 1");
+	
+	boolean[] visited = new boolean[n];
+	double totalDist = 0f;
+	
+	Pair<Double, Double> first = set.get(0); // First city
+	visited[0] = true;
+	int visitCount = 1;
+	Pair<Double, Double> temp = first;
+	
+	while (visitCount < n) {
+	    double minDist = Double.MAX_VALUE;
+	    int minIdx = 0;
+	    for (int i = 1; i < n; i++) {
+		if (visited[i]) continue;
+		double dist = getDist(temp, set.get(i));
+		if (dist < minDist) {
+		    minDist = dist;
+		    minIdx = i;
+		}
+	    }
+	    totalDist += minDist;
+	    temp = set.get(minIdx);
+	    visited[minIdx] = true;
+	    visitCount++;
+	}
+	totalDist += getDist(temp, first);
+	return totalDist;
     }
 
 }
